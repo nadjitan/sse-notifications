6c86ead171f2440700929a821047cf34e0728851
nadjitan <nadjitan@yahoo.com.ph>
2024-04-21 13:27:41 +0800

    change styles

--- app/page.tsx
+++ app/page.tsx
@@ -35,9 +35,13 @@ export default async function Home() {
   if (!user) redirect("/login")
 
   return (
-    <div className="z-10 flex flex-col gap-2">
-      <form action={createNotif}>
-        <button type="submit">create</button>
+    <div className="z-10 flex flex-col gap-4">
+      <form action={createNotif} className="self-center">
+        <button
+          type="submit"
+          className="border border-gray-400 px-2 py-1.5 w-fit rounded-md bg-black">
+          create
+        </button>
       </form>
 
       <NotifList />
